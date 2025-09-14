b49a6d8f14003dca53678981da3dc0cd9f7522ff
adonis oliveira <adonis.silva@grupolpj.com>
2025-09-13 21:25:20 -0300

    feat(): atualiza retorno da api

--- api/src/main/routes/predict_image.py
+++ api/src/main/routes/predict_image.py
@@ -38,12 +38,31 @@ async def predict(request: Request):
             raise HttpRequestError("Resposta vazia", status_code=500)
 
     except HttpRequestError as e:
-        return JSONResponse(status_code=e.status_code, content={"error": e.message})
-    
+        return JSONResponse(
+            status_code=e.status_code,
+            content={
+                "status": e.status_code,
+                "message": e.message,
+                "data": None
+            }
+        )
+
     except Exception as e:  # pylint: disable=broad-exception-caught
         response = log_and_handle_exception(e)
+        return JSONResponse(
+            status_code=response["statusCode"],
+            content={
+                "status": response["statusCode"],
+                "message": "Erro interno no servidor",
+                "data": None
+            }
+        )
 
     return JSONResponse(
-        status_code=response["statusCode"],
-        content=response["data"]
-    )
+    status_code=response["statusCode"],
+    content={
+        "status": response["statusCode"],
+        "message": response.get("message", "Operação realizada com sucesso"),
+        "data": response["data"]
+    }
+)
